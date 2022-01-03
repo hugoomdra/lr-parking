57a08255b4519afd4ad0181e875666727291c292
Hugo Madureira <hugoomdra@gmail.com>
2022-01-03 01:04:30 +0100

    add cors policy

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
-
-
-
 let app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://lr-parking.herokuapp.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 const port = process.env.PORT || 3000;
 app.listen(port);
